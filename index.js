3bfcb92cae7acdf2305b08ec384bc985c077698c
Sharon Bartov <sharonbartov@gmail.com>
2023-07-13 15:27:03 +0300

    Storing chat history

--- index.js
+++ index.js
@@ -6,25 +6,38 @@ async function main (){
     console.log(colors.bold.green('Welcome to the Chatbot Program!'))
     console.log(colors.bold.green('You can start chatting with the bot'))
 
+    //  Store conversation history
+    const chatHistory = []; 
+
     while(true) {
         const userInput = readlineSync.question(colors.yellow('You: '))
 
         try {
-         // Call the API with user input
+        // Construst messages by iterating over the history
+         const messages = chatHistory.map(([role,content]) => ({role,content}))
+
+        // Add latest user input
+         messages.push({role: 'user', content: userInput})
+
+        // Call the API with user input
          const completion = await openai.createChatCompletion({
             model: 'gpt-3.5-turbo',
-            messages: [{ role: 'user', content: userInput}]
+            messages: messages,
          })
 
         //  Get completion text/content
-        const completionText = completion.data.choices[0].message.content;
+         const completionText = completion.data.choices[0].message.content;
 
          if(userInput.toLowerCase() == 'exit') {
             console.log(colors.green('Bot: ') + completionText)
             return;
          }
 
-         console.log(colors.green('Bot: ') + completionText)
+        console.log(colors.green('Bot: ') + completionText)
+
+        //  Update history with user input and assistant response
+        chatHistory.push(['user', userInput])
+        chatHistory.push(['assistant', completionText])
         } catch (error) {
             console.error(colors.red(error))
         }
